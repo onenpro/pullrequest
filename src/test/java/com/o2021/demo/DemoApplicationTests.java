416533aace6c5212fcca60a92f359f85e63e0a2d
NoraSagarnaZ <nora.sagarna@onenpro.com>
2021-02-01 08:58:09 +0100

    Spring app

---
+++ src/test/java/com/o2021/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o2021.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
